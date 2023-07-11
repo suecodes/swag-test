60a04eafff2acc7fd2242f0c232357014879b11a
suecodes <sueoedens@gmail.com>
2023-07-11 10:18:53 -0400

    Created base project structure

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    public static void main(String args[]){
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
