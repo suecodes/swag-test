05c467ed8aaa84f1f807477970fca1baa5aab8ba
suecodes <sueoedens@gmail.com>
2023-08-10 21:13:12 -0400

    Added tests for the Cart Page to verify product list contains expected items

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 public class CartPage extends BasePage{
 
     private By labelTitle = By.className("title");
+    private By shoppingCartBadge = By.className("shopping_cart_badge");
+    private By cartItems = By.className("cart_item");
 
     public CartPage(WebDriver driver){
         super(driver);
@@ -13,6 +15,24 @@ public class CartPage extends BasePage{
 
     public String getCartPageTitle(){
         return driver.findElement(labelTitle).getText();
+    }
+
+    // Shopping cart
+    public int getCountOfItemsInShoppingCart(){
+        return Integer.parseInt(driver.findElement(shoppingCartBadge).getText());
+    }
+
+    public boolean isShoppingCartBadgeDisplayed(){
+        try {
+            return driver.findElement(shoppingCartBadge).isDisplayed();
+        }
+        catch(org.openqa.selenium.NoSuchElementException e){
+            return false;
+        }
+    }
+
+    public int getProductListCountInCart(){
+        return driver.findElements(cartItems).size();
     }
 
 }
--- src/test/java/cart/CartTests.java
+++ src/test/java/cart/CartTests.java
@@ -4,12 +4,36 @@ import base.BaseTests;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class CartTests extends BaseTests {
 
     @Test
-    public void testCartPageTitleShowsProduct(){
+    public void testCartPageTitleShowsYourCart(){
         cartPage = inventoryPage.clickShoppingCartLink();
         assertEquals(cartPage.getCartPageTitle(), "Your Cart", "Cart page title is incorrect");
+    }
+
+    @Test
+    public void testAddProductsToCartAndViewCartContainsCorrectCount(){
+        inventoryPage.clickAddBikeLightToCart();
+        inventoryPage.clickAddBackpackToCart();
+        cartPage = inventoryPage.clickShoppingCartLink();
+        assertTrue(cartPage.isShoppingCartBadgeDisplayed(), "Product not added to cart");
+        assertEquals(cartPage.getCountOfItemsInShoppingCart(), 2, "Count of items in cart is incorrect");
+    }
+
+    @Test
+    public void testCartPageShowsListOfItemsAddedToCart(){
+        inventoryPage.clickAddBackpackToCart();
+        inventoryPage.clickAddBikeLightToCart();
+        cartPage = inventoryPage.clickShoppingCartLink();
+        assertEquals(cartPage.getProductListCountInCart(), 2, "Product List on Carts page shows incorrect amount of products");
+    }
+
+    @Test
+    public void testCartPageShowsEmptyListOfItems(){
+        cartPage = inventoryPage.clickShoppingCartLink();
+        assertEquals(cartPage.getProductListCountInCart(), 0, "Product List on Carts page should be empty");
     }
 }
